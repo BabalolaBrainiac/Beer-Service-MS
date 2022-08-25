5577749a2902a6ccec15863cd6b801974d30db9e
Babalola <babalolaopedaniel@gmail.com>
2022-08-25 11:06:44 +0100

    Project Init

---
+++ src/main/java/com/babalola/beerservicems/BeerServiceMsApplication.java
@@ -0,0 +1,13 @@
+package com.babalola.beerservicems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServiceMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerServiceMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/babalola/beerservicems/BeerServiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babalola.beerservicems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
