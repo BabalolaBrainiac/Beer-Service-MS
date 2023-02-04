30568121d7e66dd353fbdcb8788d6c7d9fa95b3d
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2023-02-04 03:58:26 +0100

    adding maven and Dockerfile configuration

---
+++ target/generated-sources/annotations/com/babalola/beerservicems/mapper/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package com.babalola.beerservicems.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-02-04T03:30:15+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 19 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
---
+++ target/generated-sources/annotations/com/babalola/beerservicems/mapper/BeerMapperImpl_.java
@@ -0,0 +1,83 @@
+package com.babalola.beerservicems.mapper;
+
+import com.babalola.beerservicems.domain.Beer;
+import com.babalola.beerservicems.domain.Beer.BeerBuilder;
+import com.babalola.beerservicems.models.BeerDTO;
+import com.babalola.beerservicems.models.BeerDTO.BeerDTOBuilder;
+import com.babalola.beerservicems.models.BeerStyle;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-02-04T03:30:14+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 19 (Oracle Corporation)"
+)
+@Component
+@Qualifier("delegate")
+public class BeerMapperImpl_ implements BeerMapper {
+
+    @Override
+    public BeerDTO beerToBeerDTO(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDTOBuilder beerDTO = BeerDTO.builder();
+
+        beerDTO.id( beer.getId() );
+        if ( beer.getVersion() != null ) {
+            beerDTO.version( beer.getVersion().intValue() );
+        }
+        if ( beer.getBeerStyle() != null ) {
+            beerDTO.beerStyle( Enum.valueOf( BeerStyle.class, beer.getBeerStyle() ) );
+        }
+        beerDTO.upc( beer.getUpc() );
+        beerDTO.price( beer.getPrice() );
+
+        return beerDTO.build();
+    }
+
+    @Override
+    public BeerDTO beerToBeerDtoWithInventory(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDTOBuilder beerDTO = BeerDTO.builder();
+
+        beerDTO.id( beer.getId() );
+        if ( beer.getVersion() != null ) {
+            beerDTO.version( beer.getVersion().intValue() );
+        }
+        if ( beer.getBeerStyle() != null ) {
+            beerDTO.beerStyle( Enum.valueOf( BeerStyle.class, beer.getBeerStyle() ) );
+        }
+        beerDTO.upc( beer.getUpc() );
+        beerDTO.price( beer.getPrice() );
+
+        return beerDTO.build();
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        BeerBuilder beer = Beer.builder();
+
+        beer.id( dto.getId() );
+        if ( dto.getBeerStyle() != null ) {
+            beer.beerStyle( dto.getBeerStyle().name() );
+        }
+        if ( dto.getVersion() != null ) {
+            beer.version( dto.getVersion().longValue() );
+        }
+        beer.upc( dto.getUpc() );
+        beer.price( dto.getPrice() );
+
+        return beer.build();
+    }
+}
